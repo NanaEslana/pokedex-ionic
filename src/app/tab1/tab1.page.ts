82cdf718413d5af71451102744b168ab353937d5
Eslana Regina <reginaeslana2011@gmail.com>
2024-10-10 09:14:27 -0300

    feat: foi adicionado o texto com as habilidade de cada um

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,18 +14,21 @@ export class Tab1Page {
       name: 'Bulbasaur',
       image: 'assets/pokemon-1.png',
       type: 'Erva e Veneno',
-      description: 'Por algum tempo após o nascimento, ele usa os nutrientes contidos na semente em suas costas para crescer..'
+      description: 'Por algum tempo após o nascimento, ele usa os nutrientes contidos na semente em suas costas para crescer..',
+      ability: ' O bulbo pode absorver a luz solar, permitindo Bulbasaur para restaurar a saúde com Síntese ou disparar um poderoso Raio Solar.'
     },
     {
       name: 'Charmander',
       image: 'assets/pokemon-2.png',
       type:  'Fogo',
+      ability: 'Charmander pode produzir chamas dentro de si e projetá-las de sua boca, o que significa que ele pode respirar fogo.',
       description: 'A chama em sua cauda mostra a força de sua força vital. Se Charmander for fraco, a chama também queima fracamente.'
     },
     {
       name: 'Blastoise',
       type: 'Água',
       image: 'assets/pokemon-3.png',
+      ability: '',
       description: 'Ele aumenta deliberadamente o peso do seu corpo para poder suportar o recuo dos jatos de água que dispara.'
     }
     ,
@@ -33,6 +36,7 @@ export class Tab1Page {
       name: 'Butterfree ',
       image: 'assets/pokemon-4.png',
       type: 'Eva',
+      ability: 'As escalas impermeáveis nas asas de Butterfree são frouxamente ligado, e se a retalhos eles serão liberados para o ar.',
       description: 'Ela adora o néctar das flores e consegue localizar manchas de flores que tenham até mesmo pequenas quantidades de pólen.'
     }
     ,
@@ -40,6 +44,7 @@ export class Tab1Page {
       name: 'Rattata',
       image: 'assets/pokemon-5.png',
       type: 'Normal',
+      ability: 'Habilidade Run Away, que permite uma fuga segura de Pokémon selvagens',
       description: 'Mastigará qualquer coisa com suas presas. Se você vir um, pode ter certeza de que mais 40 vivem na área.'
     }
     ,
@@ -47,6 +52,7 @@ export class Tab1Page {
       name: 'Weedle',
       type: 'Veneno',
       image: 'assets/pokemon-6.png',
+      ability: 'A habilidade do Pokémon Weedle é Shield Dust, que protege o Pokémon dos efeitos adicionais de movimentos.',
       description: 'Cuidado com o ferrão afiado em sua cabeça. Ele se esconde na grama e arbustos onde come folhas.'
     }
     ,
@@ -54,6 +60,7 @@ export class Tab1Page {
       name: 'Charmeleon',
       type: 'Fogo',
       image: 'assets/pokemon-7.png',
+      ability: 'Quando o Charmeleon balança sua cauda, a temperatura ao redor aumenta, atormentando seus oponentes.',
       description: 'Quando ele balança sua cauda em chamas, a temperatura ao redor dele sobe cada vez mais, atormentando seus oponentes.'
     }
     ,
@@ -61,6 +68,7 @@ export class Tab1Page {
       name: 'Charizard',
       image: 'assets/pokemon-8.png',
       type: 'Fogo',
+      ability: 'Charizard pode usar uma variedade de movimentos do tipo fogo, como Explosão de Fogo. Seu fogo é tão quente que pode derreter rochas e causar incêndios florestais',
       description: 'Se Charizard ficar realmente irritado, a chama na ponta de sua cauda queima em um tom azul claro.'
     }
     ,
@@ -68,6 +76,7 @@ export class Tab1Page {
       name: 'Metapod',
       image: 'assets/pokemon-9.png',
       type: 'Eva',
+      ability: 'A habilidade do Metapod nas cartas de Pokémon Estampas Ilustradas é a de evoluir o Pokémon quando o jogador lança uma moeda e sai cara.',
       description: 'Ele está esperando o momento de evoluir. Nesse estágio, ele só pode endurecer, então ele permanece imóvel para evitar ataques.'
     }
   ]
