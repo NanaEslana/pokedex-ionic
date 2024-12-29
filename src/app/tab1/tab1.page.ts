6629ff1e980a06dcb69914a2ef31d6fbaebb0ace
Eslana Regina <reginaeslana2011@gmail.com>
2024-12-29 13:42:22 -0300

    feta: adicionei menu lateral

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -22,18 +22,12 @@ export class Tab1Page {
       evolutions: {
         start: {
           image: 'assets/pokemon-1.png',
-          name: 'Bulbasaur',
-          description: 'Por algum tempo após o nascimento, ele usa os nutrientes contidos na semente em suas costas para crescer.'
         } ,
         second: {
-          image:  'assets/evolucao/pokemon-1.2.png',
-          name: 'Ivysaur',
-          description: 'Quanto mais luz solar Ivysaur recebe, mais força brota dentro dele, permitindo que o broto em suas costas cresça mais.'
+          image:  'assets/evolucao/pokemon-1.2.png'
         },
         third: {
           image: 'assets/evolucao/pokemon-1.3.png',
-          name: 'Venusaur',
-          description: 'Enquanto se aquece no sol, ele pode converter a luz em energia. Como resultado, ele é mais potente no verão.'
         }
       }
     },
@@ -46,18 +40,30 @@ export class Tab1Page {
       evolutions: {
         start: {
           image: 'assets/pokemon-2.png',
-          name: 'Charmander',
-          description: 'A chama em sua cauda mostra a força de sua força vital. Se Charmander for fraco, a chama também queima fracamente.'
         } ,
         second: {
           image:  'assets/evolucao/pokemon-2.2.png',
-          name: 'Charmeleon',
-          description: 'Quando ele balança sua cauda em chamas, a temperatura ao redor dele sobe cada vez mais, atormentando seus oponentes.'
         },
         third: {
           image: 'assets/evolucao/pokemon-3.2.png',
-          name: 'Charizard',
-          description: 'Se Charizard ficar realmente irritado, a chama na ponta de sua cauda queima em um tom azul claro.'
+        }
+      }
+    },
+    {
+      name: 'Pikachu',
+      image: 'assets/pokemon3.png',
+      type: 'Eletrico',
+      ability: 'Charmander pode produzir chamas dentro de si e projetá-las de sua boca, o que significa que ele pode respirar fogo.',
+      description: 'Quando fica irritado, ele imediatamente descarrega a energia armazenada nas bolsas em suas bochechas.',
+      evolutions: {
+        start: {
+          image: 'assets/pokemon3.png',
+        } ,
+        second: {
+          image:  'assets/evolucao/pokemon-2.2.png',
+        },
+        third: {
+          image: 'assets/evolucao/pokemon-3.2.png',
         }
       }
     }
